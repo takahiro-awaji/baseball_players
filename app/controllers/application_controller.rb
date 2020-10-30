ff911eb1be0b173a85ad22b971a1bc28ac6ca51a
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-30 18:32:33 +0900

    チーム カラム変更に伴うコントローラー、ビュー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :url, :activity_base_id, :attribute_id,
-                                                       :level_id, :player_age_id, :player_history_id, :league, :title,
-                                                       :since_id, :activity_pace_id, :games_per_year_id, :slogan, 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :team_url, :activity_base_id, :team_attribute_id,
+                                                       :team_level_id, :team_player_age_id, :team_player_history_id, :league, :team_title,
+                                                       :since_id, :activity_pace_id, :games_per_year_id, :team_slogan, 
                                                        :introduction, activity_day:[]])
   end
 end
