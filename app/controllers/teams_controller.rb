a07f771453c40a61cff9fbb72d648cb59fa699ad
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-30 21:58:12 +0900

    コントローラー作成、show画面へのリンク追加

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,36 @@
 class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update]
+
   def index
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @team.update(team_params)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    team = Team.find(params[:id])
+    team.destroy
+  end
+
+  private
+
+  def set_team
     @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:team_name, :team_name_kana, :activity_base_id, :team_attribute_id, :team_level_id,
+                          :team_player_age_id, :team_player_history_id, :league, :team_title, :since_id, 
+                          :activity_pace_id, :games_per_year_id, :team_slogan, :introduction, activity_day: [])
   end
 end
