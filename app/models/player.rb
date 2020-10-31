740e2fbd268fd7a85f3de9e5cf99b53c65e80875
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-31 18:52:46 +0900

    選手 モデル、バリデーション設定

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,8 @@
 class Player < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :position
   belongs_to :team
+
+  validates :name, presence: true
+  validates :main_position_id, presence: true, numericality: { other_than: 0, message: 'Select' }
 end
