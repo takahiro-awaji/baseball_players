41b598ecaa68f8ed4566b74d477bbdd70c7f4991
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-10-31 18:44:06 +0900

    active_hash position作成

---
+++ app/models/position.rb
@@ -0,0 +1,10 @@
+class Position < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' }, 
+    { id: 1, name: '投手' },
+    { id: 2, name: '捕手' },
+    { id: 3, name: '内野手' },
+    { id: 4, name: '外野手' },
+    { id: 5, name: 'スタッフ' }
+    ]
+end
