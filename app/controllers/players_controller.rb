43f445d9d9c5153333b7588cfbc48a7126fbbd7a
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-11-01 16:58:36 +0900

    コントローラーにedit, update, destroyアクション追加、before_actionの定義

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,28 +1,39 @@
 class PlayersController < ApplicationController
-
+  before_action :set_player, only: [:show, :edit, :update]
+  before_action :set_team
   def index
-    @team = Team.find(params[:team_id])
     @players = Player.where(team_id: @team.id)
   end
 
   def new
     @player = Player.new
-    @team = Team.find(params[:team_id])
   end
   
   def create
-    @team = Team.find(params[:team_id])
     @player = Player.new(player_params)
     if @player.save
-      redirect_to "/teams/#{@team.id}"
+      redirect_to "/teams/#{@team.id}/players"
     else
       render 'new'
     end
   end
 
   def show
-    @team = Team.find(params[:team_id])
-    @player = Player.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @player.update(player_params)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    player = Player.find(params[:id])
+    player.destroy
   end
 
   private
@@ -30,6 +41,14 @@ class PlayersController < ApplicationController
   def player_params
     params.require(:player).permit(:name, :sex, :birthday, :main_position_id, :number, :at_pitch, :at_bat,
                                    :sub_position, :player_post, :staff_post, :carrer, :promotion).merge(team_id: current_team.id)
+  end
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
+
+  def set_team
+    @team = Team.find(params[:team_id])
   end
 
 end
