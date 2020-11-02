5b44381eaa18064b0915a93551bc558fb30ed733
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-11-02 20:19:41 +0900

    コントローラー作成

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,29 @@
+class GamesController < ApplicationController
+  before_action :set_team, only: [:new, :create]
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to "/teams/#{@team.id}"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
+
+  def game_params
+    params.require(:game).permit(:gameday, :gametime, :opponent, :tournament_name, :field_name,
+                                 :game_attribute, :total_stats_add, :top_or_bottom, :game_myteam_score, :game_opponent_score, 
+                                 :game_result, :game_comment).merge(team_id: current_team.id)
+  end
+
+end
