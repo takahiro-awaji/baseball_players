7a59357bc12877742d6753f0cdb13b3b4b1b1a88
takahiro-awaji <69838495+takahiro-awaji@users.noreply.github.com>
2020-12-03 22:34:14 +0900

    Chart.js 入力データの表示

--- app/javascript/radar_batter.js
+++ app/javascript/radar_batter.js
@@ -2,9 +2,14 @@ const radarChartData = {
   labels: ["打撃", "パワー", "走力", "肩力", "守備力"],
   datasets: [
     {
-      fillColor : /*"#f2dae8"*/"rgba(242,218,232,0.6)",
-      strokeColor : /*"#dd9cb4"*/"rgba(221,156,180,0.6)",
-      data : [100,100,100,100,100]
+      fillColor : /*"#f2dae8"*/"rgba(0,128,0,0.6)",
+      strokeColor : /*"#dd9cb4"*/"rgba(144,238,144,0.6)",
+      data : [gon.player.batting,
+              gon.player.power,
+              gon.player.running,
+              gon.player.throwing,
+              gon.player.fielding
+      ]
     }
   ]
   };
--- app/javascript/radar_pitcher.js
+++ app/javascript/radar_pitcher.js
@@ -1,16 +1,31 @@
 const radarChartData = {
-  labels: ["球速", "スタミナ", "コントロール", "変化球", "守備力"],
+  labels: ["球速", "コントロール", "スタミナ", "変化球", "守備力"],
   datasets: [
     {
-      fillColor : /*"#f2dae8"*/"rgba(242,218,232,0.6)",
-      strokeColor : /*"#dd9cb4"*/"rgba(221,156,180,0.6)",
-      data : [80,60,40,100,100]
+      fillColor : /*"#f2dae8"*/"rgba(255,192,203,0.6)",
+      strokeColor : /*"#dd9cb4"*/"rgba(255,182,193,0.6)",
+      data : [gon.player.speedball,
+              gon.player.control,
+              gon.player.stamina,
+              gon.player.breakingball,
+              gon.player.fielding_of_pitcher
+      ],
     }
   ]
   };
+
+  const options = {
+    scale: {
+      ticks: {
+        max: 100,
+        stepSize: 20
+      }
+    }
+  }
   
   window.addEventListener("load", function(){
   window.myRadar = new Chart(document.getElementById("radar-chart-pitcher").getContext("2d")).Radar(radarChartData, {
-    responsive: true
+    responsive: true,
+    stepSize: 10
   });
   });
